a4837d543d4a4edbd63e34e6425ac70fb8b8697e
Warnigo Warfurious <abumain0012@gmail.com>
2024-08-24 11:59:20 +0500

    [eslint]: prettier, eslint, husky intall and connected

---
+++ scripts/config.mjs
@@ -0,0 +1,6 @@
+export const config = {
+  name: '\x77\x61\x72\x6e\x69\x67\x6f\n',
+  ascii:
+    '__  __  __\n\u005C \u005C \u005C \u005C \u005C\u2215\n \u005C \u005C\u2215\u005C \u005C\n  \u005C\u2215  \u005C\u2215\n',
+  build: '\u0042 \u0055 \u0049 \u004C \u0044\n',
+}
